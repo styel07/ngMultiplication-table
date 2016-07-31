c10ac9e4063ec1ab319998ed2da1d3a08d2b9d12
Eleazar Corpuz <eleazarcorpuz@gmail.com>
2016-07-31 00:43:16 -0700

    add compute function to script.js to compute numbers A and B

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,18 @@
 // angular modules
 angular.module('myApp', [])
-.controller('myController', function() {
+.controller('myController', function($attrs) {
   var vm = this;
-  vm.numbers = [1,2,3,4,5,6,7,8,9,10];
+  vm.numberLimit = $attrs.initialNumberLimit || 10;
+
+  vm.numbers = function() {
+      var numbers = [];
+      for(var i=0; i < vm.numberLimit; i++) {
+          numbers[i] = i + 1;
+      }
+      return numbers;
+  };
+
+  vm.compute = function(a,b) {
+    return a * b;
+  };
 });
